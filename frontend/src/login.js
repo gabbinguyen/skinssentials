efbb25ce3ef65e52064a407285c2fdb9b01ca25d
Gabbi Nguyen <gabbinguyen@gmail.com>
2020-12-03 17:14:11 -0600

    modal button

---
+++ frontend/src/login.js
@@ -0,0 +1,40 @@
+// const loginBtn = document.querySelector("#.button")
+// loginBtn.addEventListener('click', (event) => {
+//     document.querySelector(".popup")
+
+// })
+
+// function openForm() {
+//     document.getElementById("loginForm").style.display = "block";
+//   }
+  
+//   function closeForm() {
+//     document.getElementById("myForm").style.display = "none";
+//   }
+
+
+  // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
