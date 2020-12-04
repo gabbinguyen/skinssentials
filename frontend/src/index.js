eae4f4a56f5a2086f342683506815a43919e3515
sarabastian <seb118@georgetown.edu>
2020-12-04 11:22:23 -0500

    userProduct fetch statement

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -35,6 +35,7 @@ function renderProduct(product){
         
             <button type="button" id="product-button-${product.id}"
              class="btn btn-primary">Add to Cabinet</button>
+             
         </div>
         </div
     `
@@ -45,7 +46,8 @@ function renderProduct(product){
     const addToCabinetButton = document.getElementById(`product-button-${product.id}`)
     addToCabinetButton.addEventListener('click', () => {
         
-        fetchCabinet(product)
+        
+        fetchUserProducts()
     })
     
 
@@ -53,45 +55,91 @@ function renderProduct(product){
 
 
 
-function fetchCabinet(product) {
+// function fetchCabinet(product) {
 
-    const data = {
-        user_id: currentUser.id,
-        product_id: product.id
+//     const data = {
+//         user_id: currentUser.id,
+//         product_id: product.id
         
-    }
+//     }
 
-    fetch('http://localhost:3000/api/v1/user_products', {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json',
-            Accept: 'application/json'
-        },
-        body: JSON.stringify(data)
+//     fetch('http://localhost:3000/api/v1/user_products', {
+//         method: "POST",
+//         headers: {
+//             'Content-Type': 'application/json',
+//             Accept: 'application/json'
+//         },
+//         body: JSON.stringify(data)
     
-    .then(r => r.json())
-    .then(json => json.forEach(cabinet => {
-       console.log(cabinet)
-       renderCabinet(cabinet)
-    }))
+//     .then(r => r.json())
+//     .then(json => json.forEach(cabinet => {
+//        console.log(cabinet)
+//        renderCabinet(cabinet)
+//     }))
     
-})
-}
+// })
+// }
 
-function renderCabinet(cabinet){
-    console.log(cabinet)
+// function renderCabinet(cabinet){
+    
+   
+//     const CabinetCardsContainer = document.getElementById("cabinet-cards-container")
+//     const newCabinetItem = document.createElement("div")
+    
+//     modalBody.innerHTML = ''
+//     modalBody.innerHTML =
+//     `
+   
+//     <div class="card-body">
+//         <h5 class="card-title">${cabinet.id}</h5>
+//         <h6 class="card-subtitle mb-2 text-muted"> By: </h6>
+//         <p class="card-text" style="font-size: 12px; letter-spacing: 1px;"></p>
+//         <button type="button" id="remove-button-${cabinet.id}" class="btn btn-danger">Remove</button>
+//     </div>
+//     `
+//     CabinetCardsContainer.append(modalBody)
+   
+// }
+
+
+function fetchUserProducts() {
+   
+    
+    const userProductsURL = 'http://localhost:3000/api/v1/user_products';
+    fetch(userProductsURL)
+      .then(res => res.json())
+      .then(json => json.forEach(userProduct => {
+      
+        renderUserProduct(userProduct)} ) 
+      )}
+      
+
+function renderUserProduct(userProduct){
+    const x = userProduct.name.skin_type
+    console.log(x)
     const CabinetCardsContainer = document.getElementById("cabinet-cards-container")
-    const newCabinetItem = document.createElement("div")
-    
-    newCabinetItem.innerHTML = `
-   
-    <div class="card-body">
-        <h5 class="card-title">${cabinet.id}</h5>
-        <h6 class="card-subtitle mb-2 text-muted"> By: </h6>
-        <p class="card-text" style="font-size: 12px; letter-spacing: 1px;"></p>
-        <button type="button" id="remove-button-${cabinet.id}" class="btn btn-danger">Remove</button>
-    </div>
+    const userProductCard = document.createElement("div")
+    userProductCard.innerHTML = ''
+    userProductCard.className = 'card'
+    userProductCard.style = "width: 18rem;"
+    userProductCard.id = `user-product-card-${userProduct.id}`
+    userProductCard.innerHTML = `
+        <img src="${userProduct}" class="card-img-top" alt="Card image cap">
+        <div class="card-body">
+        <h4 class="card-title">${userProduct}</h4>
+        <h6 class="card-subtitle mb-2 text-muted"${userProduct}</h6>
+        <p class="card-text">
+            <li> For: ${userProduct} Skin </li>
+            <li> Step: ${userProduct} </li></ul></p>
+        
+            <button type="button" id="product-button-${userProduct.id}"
+             class="btn btn-primary">Add to Cabinet</button>
+             
+        </div>
+        </div
     `
-    CabinetCardsContainer.append(newCabinetItem)
-   
-}
+    CabinetCardsContainer.append(userProductCard)
+    // document.querySelector('.btn-primary').style.display = "none";
+
+
+    }
